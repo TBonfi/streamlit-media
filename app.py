da8018dda16d02c99e28a760566c8d37bc90e29a
TBonfi <bonfiglio.tomas@gmail.com>
2024-01-29 20:22:46 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+
+# datos sample
+data = {
+    'Name': ['patas', 'proteina', 'orejas'],
+    'Image_Path': ['images/patas.jpg',
+                   'images/protein.jpg',
+                   'images/orejas.jpg'],
+    'Description': ['Description1', 'Description2', 'Description3'],
+    'Image': [True, False, True]
+}
+
+df = pd.DataFrame(data)
+
+# Título del app
+st.title('Search and Display Images App')
+
+# Buscador en la barra (en realidad es para tener un input con el cual filtrar)
+search_term = st.sidebar.text_input("Search by Name", "")
+
+# Ahora utilizamos la variable search_term para filtrar, es para una prueba de concepto
+filtered_df = df[df['Name'].str.contains(search_term, case=False)]
+
+# Display search results
+if not filtered_df.empty:
+    # selectbox es la "caja" de búsqueda que se puede desplegar, tendrá los resultados filtrados según la query anterior
+    selected_item = st.selectbox('Select an item:', filtered_df['Name'])
+
+
+    selected_row = filtered_df[filtered_df['Name'] == selected_item].iloc[0]
+
+    st.write('**Name:**', selected_row['Name'])
+    st.write('**Description:**', selected_row['Description'])
+    st.image(selected_row['Image_Path'], caption='Selected Image', use_column_width=True)
+
+else:
+    st.warning('No results found.')
+
+
+st.subheader('Entire DataFrame')
+st.write(df)
