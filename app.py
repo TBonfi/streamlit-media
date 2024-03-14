f8487b959511fe40f077158d01d7a22e8adc88c0
TBonfi <bonfiglio.tomas@gmail.com>
2024-03-14 20:25:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+'''
+Para ejecutar la aplicación-> abrir una CMD y ejecutar: streamlit run app.py
+Para encontrar donde está el config.toml -> streamlit config show (E.G.: "C:\Users\bonfi\.streamlit\config.toml")
+Para cortar la ejecución -> ctrl+c
+'''
+
 # datos sample
 data = {
     'Name': ['patas', 'proteina', 'orejas'],
